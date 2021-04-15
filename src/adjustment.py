a95ef758308170bdc8fdc3343c4db2e7f7a808ee
Joe Bak-Coleman <joebak@uw.edu>
2021-04-15 18:56:33 +0000

    Commented adjustment.py

--- src/adjustment.py
+++ src/adjustment.py
@@ -3,12 +3,32 @@ import pandas as pd
 import pickle
 
 def get_adjusted_df(events, incidents,root='.'):
+    """A function to return a dataframe to link within-largest-event 
+    tweets to subsequent tweets. 
+    
+    Parameters
+    ----------
+    events : pandas dataframe
+        Our dataframe of all events. 
+    incidents : pandas dataframe
+        Our dataframe of all incidents. 
+
+    Returns
+    -------
+    max_events_incidents
+        Dataframe of only the largest events from each incident.
+    """
+    
+    #Gropuby incident and pull the largest. 
     temp = events[events['included']]
     idx = temp.groupby(['incident'])['observed_engagement'].transform(max) == temp['observed_engagement']
     max_events = temp[idx]
+    
+    #Merge largest events with incident-related data. 
     max_events_incidents = pd.merge(max_events, incidents, left_on=['incident_name'], 
                                     right_on=['incident_name'])
 
+    #Count tweets before during and after the event
     before = []
     after = []
     durings =[]
@@ -19,13 +39,21 @@ def get_adjusted_df(events, incidents,root='.'):
         after.append(raw_df.iloc[row[1]['end']:]['total_tweets'].sum())
         before.append(raw_df.iloc[:row[1]['start']]['total_tweets'].sum())
         durings.append(raw_df.iloc[row[1]['start']:row[1]['end']]['total_tweets'].sum())
-
+    
+    #Assign those values to the dataframe
     max_events_incidents['before'] = before
     max_events_incidents['after'] =after
     max_events_incidents['during'] =durings
     return max_events_incidents
 
 def print_engagment(max_events_incidents):
+    """A function to print out engagement before the largest events, 
+    during the largest events, and afterwards. 
+    Parameters
+    -------
+    max_events_incidents
+        Dataframe of only the largest events from each incident.
+    """
     rounder = lambda x: '{:0.2f}'.format(x*100)
     total = max_events_incidents['before'].sum() + \
         max_events_incidents['during'].sum() + \
@@ -48,10 +76,29 @@ def print_engagment(max_events_incidents):
     print(rounder(np.sum(max_events_incidents['after'])/total ))
     
 def sample_neg_binom(model, max_events_incidents):
+    """Wrapper for sampling from our model (neg_binom.stan) using 
+    the data from the largest-per-incident events.
+    
+    Parameters
+    ----------
+    model : pystan model
+        compiled model from src/neg_binom.stan
+    max_events_incidents: pandas dataframe
+        Dataframe of only the largest events from each incident.
+
+    Returns
+    -------
+    samples : Pystan sampling object
+    """
+    #Define some values to simulate in generated quantities block
     x_sim = np.logspace(np.log(np.min(max_events_incidents['observed_engagement'])-10),
                     np.log(np.max(max_events_incidents['observed_engagement'])+20),20,base=np.e)
+    
+    #Pull after (y) and during (x)
     after = max_events_incidents['after']
     during = np.log(max_events_incidents['observed_engagement'])
+   
+    #Sample
     samples = model.sampling(data = {'after':after.values.astype('int'), 
                                            'during':during.values, 
                                             'N':during.size, 
@@ -60,6 +107,26 @@ def sample_neg_binom(model, max_events_incidents):
     return samples
 
 def get_plot_df(order, max_events_incidents,sim_df, samples, root='.'):
+    
+    """Wrapper for sampling from our model (neg_binom.stan) using 
+    the data from the largest-per-incident events.
+    
+    Parameters
+    ----------
+    order: list 
+        specifies which rows from sim_df to plot
+    max_events_incidents: pandas dataframe
+        Dataframe of only the largest events from each incident.
+    sim_df
+        Dataframe specifying simulation conditions and containing the rows in order.
+    root 
+        Root location for the analysis
+
+    Returns
+    -------
+    samples : Pystan sampling object
+    """
+    
     colors  = []
     plot_dfs = []
 
